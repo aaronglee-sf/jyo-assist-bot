d3e9b94832b6f19d36dccd9d02698be55f1f02ac
aaronglee-sf <aaronglee@gmail.com>
2026-09-11 21:24:26 -0700

    Create gym-access.js

---
+++ knowledge/gym-access.js
@@ -0,0 +1,11 @@
+For any San Mateo High School Gyms (Hillsdale, Burlingame, Cap, Aragon, Mills) --
+- Gym Doors will be open for you, but they will only open up at the time of our Practice. If you are 10-15 Minutes early, the gym may not be open because it is not our reserved time.
+- If it is our reserved time, and the gym is not open, please call Rodrigo Del Toro at 650-280-8130. Tell him that you are part of JYO and we have our reservation starting at X time, per your schedule.
+
+For Brewer Gyms -
+- For the 12:30PM Slot, Coach Brian (1st Grade Boys) has the Key to get in the Gym.
+- For the 4:30PM Slot, there is another team in the gym prior, so the gym will be open for you.
+
+For Crocker Gyms --
+- For 4/5 & 7th Boys Saturday Practices, Coach Alan (4/5 Grade Boys) has the Key to unlock the Gym.
+- For 3rd Girls Practices, Team Manager Lindsey has the Key to unlock the Gym.
